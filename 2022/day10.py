1a18edc38296e4af4873f3da1edac188b3380e12
FrenchCommando <martialren@gmail.com>
2022-12-28 11:04:28 -0500

    end day10

--- 2022/day10.py
+++ 2022/day10.py
@@ -43,3 +43,41 @@ signal_map = {
 }
 display(signal_map)
 display(sum(c * v for c, v in signal_map.items()))
+
+# second version
+register_value = 1
+l_cycle = [register_value, register_value]
+for line in lines:
+    l_cycle.append(register_value)
+    if line == "noop":
+        pass
+    else:
+        d_group = re.match(
+            pattern=r"addx (?P<value>.*)",
+            string=line,
+        ).groupdict()
+        value = int(d_group['value'])
+        register_value += value
+        l_cycle.append(register_value)
+display(l_cycle)
+signal_map = {
+    cycle_value: l_cycle[cycle_value] for cycle_value in [20, 60, 100, 140, 180, 220]
+}
+display(signal_map)
+display(sum(c * v for c, v in signal_map.items()))
+
+l_pixels = [
+    l_cycle[i] in [i % 40, (i - 1) % 40, (i - 2) % 40]
+    for i in range(len(l_cycle))
+]
+display(l_pixels)
+
+
+p_iter = iter(l_pixels)
+next(p_iter)
+pixels = [
+    "".join(('#' if pp else ".") for pp in p) for p in zip(*[p_iter] * 40)
+]
+display(pixels)
+for pixel_line in pixels:
+    print(pixel_line)
