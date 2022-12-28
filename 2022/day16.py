45f97bc594249bed81d4ad481be399d9103138bf
FrenchCommando <martialren@gmail.com>
2022-12-28 18:19:58 -0500

    day16 parsed

--- 2022/day16.py
+++ 2022/day16.py
@@ -1,3 +1,4 @@
+import re
 from utils.printing import display
 
 
@@ -7,3 +8,30 @@ with open("day16.txt", "r") as f:
 
 lines = [line.strip() for line in lines]
 display(lines)
+
+
+class Valve:
+    def __init__(self, valves, flow):
+        self.valves = valves
+        self.flow = flow
+
+    def __repr__(self):
+        return f"Valve:\t{self.valves=}\t{self.flow=}"
+
+
+d_valves = dict()
+
+
+for line in lines:
+    d_group = re.match(
+        pattern=r"Valve (?P<name>.*) has flow rate=(?P<flow>.*); "
+                r"tunnel(s?) lead(s?) to valve(s?) "
+                r"(?P<valves>.*)",
+        string=line,
+    ).groupdict()
+    d_valves[d_group['name']] = Valve(valves=d_group['valves'].split(", "), flow=int(d_group['flow']))
+
+display(d_valves)
+
+n_minutes = 30
+print("The End")
