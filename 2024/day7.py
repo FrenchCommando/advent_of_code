9a89ce1992a8be79f69f494889680e952996606c
FrenchCommando <martialren@gmail.com>
2024-12-18 18:39:35 -0500

    2024 day7

--- 2024/day7.py
+++ 2024/day7.py
@@ -1,4 +1,6 @@
 import re
+from queue import Queue
+
 from utils.printing import display
 
 example = """190: 10 19
@@ -19,7 +21,35 @@ with open("day7.txt", "r") as f:
 
 
 def count(l):
-    return 1
+    c = []
+    for line in l:
+        # print(line)
+        line_s, line_n = line.split(": ")
+        nums = list(map(int, line_n.split(" ")))
+        v = int(line_s)
+        if solve(v, nums):
+            # print(v, nums)
+            c.append(v)
+    print(c)
+    return sum(c)
+
+
+def solve(v, nums):
+    q = Queue()
+    q.put((v, nums))
+    while not q.empty():
+        val, n = q.get()
+        if len(n) == 1:
+            if val == n[0]:
+                return True
+        else:
+            nn = n[:-1]
+            last = n[-1]
+            if val > last:
+                q.put((val - last, nn))
+            if val % last == 0:
+                q.put((val // last, nn))
+    return False
 
 
 p = count(l=example.split("\n"))
@@ -29,7 +59,38 @@ print("count", ps)
 
 
 def count2(l):
-    return 1
+    c = []
+    for line in l:
+        # print(line)
+        line_s, line_n = line.split(": ")
+        nums = list(map(int, line_n.split(" ")))
+        v = int(line_s)
+        if solve2(v, nums):
+            # print(v, nums)
+            c.append(v)
+    print(c)
+    return sum(c)
+
+
+def solve2(v, nums):
+    q = Queue()
+    q.put((v, nums))
+    while not q.empty():
+        val, n = q.get()
+        if len(n) == 1:
+            if val == n[0]:
+                return True
+        else:
+            nn = n[:-1]
+            last = n[-1]
+            if val > last:
+                q.put((val - last, nn))
+            if val % last == 0:
+                q.put((val // last, nn))
+            if str(val).endswith(str(last)) and len(str(val)) > len(str(last)):
+                # print(val, last, nn)
+                q.put((int(str(val)[:-len(str(last))]), nn))
+    return False
 
 
 p2 = count2(l=example.split("\n"))
