0929cdc78d45d1f702a906d7b851bea5f9abc73d
FrenchCommando <martialren@gmail.com>
2024-12-23 21:42:47 -0500

    2024 day14

--- 2024/day14.py
+++ 2024/day14.py
@@ -1,4 +1,6 @@
 import re
+import time
+
 from utils.printing import display
 
 example = """p=0,4 v=3,-3
@@ -21,21 +23,63 @@ with open("day14.txt", "r") as f:
     display(s)
 
 
-def count(l):
-    return 1
+def count(l, n, a, b):
+    problems = []
+    for m in re.findall(r"p=(\-?\d+),(\-?\d+) v=(\-?\d+),(\-?\d+)", l):
+        problems.append(tuple(map(int, m)))
+    # print(problems)
+    destination = []
+    for problem in problems:
+        x = (problem[0] + n * problem[2]) % a
+        y = (problem[1] + n * problem[3]) % b
+        # print(problem)
+        destination.append((x, y))
+    counts = [[0, 0], [0, 0]]
+    for x, y in destination:
+        if x == a // 2 or y == b // 2:
+            continue
+
+        qx = (x - 1) // (a // 2) if x != 0 else 0
+        qy = (y - 1) // (b // 2) if y != 0 else 0
+        # print(qx, qy, x, y, a, b)
+        counts[qx][qy] +=1
+    print(counts)
+    return counts[0][0] * counts[0][1] * counts[1][0] * counts[1][1]
 
 
-p = count(l=example.split("\n"))
+p = count(l=example.replace("\n", ""), n=100, a=11, b=7)
 print("count", p)
-ps = count(l=[line.strip() for line in s])
+print()
+ps = count(l="".join(s).replace("\n", ""), n=100, a=101, b=103)
 print("count", ps)
 
 
-def count2(l):
-    return 1
+def count2(l, n, a, b):
+    problems = []
+    for m in re.findall(r"p=(\-?\d+),(\-?\d+) v=(\-?\d+),(\-?\d+)", l):
+        problems.append(tuple(map(int, m)))
+    # print(problems)
+    while True:
+        if n % 10000 == 0:
+            print(n)
+        destination = []
+        for problem in problems:
+            x = (problem[0] + n * problem[2]) % a
+            y = (problem[1] + n * problem[3]) % b
+            # print(problem)
+            destination.append((x, y))
+
+        grid = [[0 for j in range(b)] for i in range(a)]
+        for x, y in destination:
+            grid[x][y] += 1
+        k = 34
+        cc = [sum(u) for u in grid]
+        if any(u > k for u in cc):
+            for line in grid[15:53]:  # [-38:]:
+                print("".join(map(str, line)).replace("0", " "))
+            print(n)
+            time.sleep(0.5)
+        n += 1
 
 
-p2 = count2(l=example.split("\n"))
-print("count2", p2)
-ps2 = count2(l=[line.strip() for line in s])
-print("count2", ps2)
+count2(l="".join(s).replace("\n", ""), n=0, a=101, b=103)
