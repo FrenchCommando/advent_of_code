aeefe390ddacf72e793b5227f86578749153d018
FrenchCommando <martialren@gmail.com>
2024-12-25 12:05:58 -0500

    2024 day25

--- 2024/day25.py
+++ 2024/day25.py
@@ -1,7 +1,45 @@
 import re
 from utils.printing import display
 
-example = """"""
+example = """#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####"""
 
 
 
@@ -11,20 +49,43 @@ with open("day25.txt", "r") as f:
 
 
 def count(l):
-    return 1
+    i = iter(l)
+    locks = []
+    keys = []
+    try:
+        while True:
+            top = next(i)
+            is_lock = all(u[0] == "#" for u in top)
+            c = "#" if is_lock else "."
+            item = [0 for k in range(5)]
+            while True:
+                line = next(i)
+                if not line:
+                    break
+                for k in range(5):
+                    if line[k] == c:
+                        item[k] += 1
+            if is_lock:
+                locks.append(item)
+            else:
+                keys.append(item)
+    except StopIteration:
+        if is_lock:
+            locks.append(item)
+        else:
+            keys.append(item)
+    print("Locks", locks)
+    print("Keys", keys)
+
+    couple = 0
+    for lock in locks:
+        for key in keys:
+            if all(lock[k] <= key[k] for k in range(5)):
+                couple += 1
+    return couple
 
 
 p = count(l=example.split("\n"))
 print("count", p)
 ps = count(l=[line.strip() for line in s])
 print("count", ps)
-
-
-def count2(l):
-    return 1
-
-
-p2 = count2(l=example.split("\n"))
-print("count2", p2)
-ps2 = count2(l=[line.strip() for line in s])
-print("count2", ps2)
