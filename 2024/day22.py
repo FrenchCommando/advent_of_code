0d172c8bb74185f585d9e60474fa1b85abb38110
FrenchCommando <martialren@gmail.com>
2024-12-30 19:28:34 -0500

    2024 day22 part2

--- 2024/day22.py
+++ 2024/day22.py
@@ -42,11 +42,50 @@ ps = count(l=[line.strip() for line in s])
 print("count", ps)
 
 
+def bananas(index, number, skip, container):
+    last_number = number % 10
+    increments = []
+    for turn in range(skip):
+        v = number * 64
+        number = number ^ v
+        number = number % 16777216
+        v = number // 32
+        number = number ^ v
+        number = number % 16777216
+        v = number * 2048
+        number = number ^ v
+        number = number % 16777216
+        if last_number is not None:
+            number_trim = number % 10
+            increments.append(number_trim - last_number)
+            if len(increments) >= 4:
+                sequence = tuple(increments[-4:])
+                # print(sequence)
+                if sequence not in container:
+                    container[sequence] = dict()
+                if index not in container[sequence]:
+                    container[sequence][index] = number_trim
+        last_number = number_trim
+
+
 def count2(l):
-    return 1
+    print(l)
+    container = dict()
+
+    for index, line in enumerate(l):
+        number = int(line)
+        bananas(index=index, number=number, skip=2000, container=container)
+
+    n_bananas = {b: sum(v.values()) for b, v in container.items()}
+
+    # print(container)
+    print(container.get((-2,1,-1,3), None))
+    # print(n_bananas)
+    return max(n_bananas.values())
 
 
-p2 = count2(l=example.split("\n"))
-print("count2", p2)
+p200 = count2(l=['123'])
+p20 = count2(l=['1', '2', '3', '2024'])
+print("count2", p200, p20)
 ps2 = count2(l=[line.strip() for line in s])
 print("count2", ps2)
